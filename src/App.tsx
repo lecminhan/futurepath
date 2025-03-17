70769c0142f78be1ace18867d986ea3ef85a7f92
minhan1811 <lecminhan@dtu.edu.vn>
2025-03-17 14:41:39 +0700

    Core of FuturePath project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Hello, Bootstrap! 🚀</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  )
+}
+
+export default App
